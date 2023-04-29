e17c39ce87df5818890e15d837b75fc51030ebf3
Shampa Shaw <shampashaw088@gmail.com>
2023-04-29 15:37:48 +0530

    backend

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import user from './user';
+import pin from './pin';
+import postedBy from './postedBy';
+import save from './save';
+import comment from './comment';
+
+export const schemaTypes = [user, pin, comment, postedBy, save]
